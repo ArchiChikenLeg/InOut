4221684c830e5bad8edfd2d56f3afc7f52058d6d
ArchiChikenLeg <andrienkolidia@gmail.com>
2022-02-20 12:07:45 +0200

    Done

---
+++ 7.java
@@ -0,0 +1,15 @@
+package InOut;
+
+import java.util.Scanner;
+
+public class InOutClass {
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter number:");
+		int n = in.nextInt();
+		
+		
+		System.out.printf("Number of decades is %d", (n/10)%10);
+		in.close();
+	}
+}
